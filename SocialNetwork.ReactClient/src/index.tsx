3acf997d44bf3f9352ba98e0a20aa953ff1c4ee4
Niraj Patel <nirajp82@yahoo.com>
2020-06-06 16:19:19 -0400

    Added Widget and Date-FNS Packages

--- SocialNetwork.ReactClient/src/index.tsx
+++ SocialNetwork.ReactClient/src/index.tsx
@@ -1,13 +1,21 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
+
+import dateFnsLocalizer from 'react-widgets-date-fns';
+
 import 'mobx-react-lite/batchingForReactDom';
-import ScrollToTop from './layout/ScrollToTop';
-import './layout/styles.css';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-widgets/dist/css/react-widgets.css';
+
+
+import './layout/styles.css';
+
+import ScrollToTop from './layout/ScrollToTop';
 import createBrowserHistory from './utils/createBrowserHistory';
 import App from './layout/App';
 
+dateFnsLocalizer();
 
 ReactDOM.render(
     <Router history={createBrowserHistory}>
