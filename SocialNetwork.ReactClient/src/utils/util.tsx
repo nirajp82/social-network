cd3fc81fd7b662bb69347fb020c02451b06f757c
Niraj Patel <nirajp82@yahoo.com>
2020-07-06 17:46:27 -0400

    Fixed DateTime Function

--- SocialNetwork.ReactClient/src/utils/util.tsx
+++ SocialNetwork.ReactClient/src/utils/util.tsx
@@ -1,21 +1,35 @@
 ﻿//import moment from "moment";
 
 export const combineDateAndTime = (date: Date, time: Date) => {
-    //const year = date.getFullYear();
-    //const month = date.getMonth() + 1;
-    //const day = date.getDate();
-    const dateString = date.toISOString().split('T')[0];
-    const timeString = time.toISOString().split('T')[1];
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const dateString = `${year}-${month}-${day}`;
+    const timeString = time.getHours() + ':' + time.getMinutes() + ':00';
+    const dt = new Date(dateString + ' ' + timeString);
 
-    const dtIso = new Date(dateString + 'T' + timeString);
-    return dtIso;
-    //console.log(dateString + 'T' + timeString);
-
-    //console.log(moment(date.toISOString()).format('YYYY-MM-DD') + 'T' + moment(time.toISOString()).format('HH:mm:SS') + '.000Z');
-
-    //const dtLocal = new Date(moment(date).format('YYYY-MM-DD') + 'T' + moment(time.toISOString()).format('HH:mm:SS') + '.000Z');
-
-    ////const dtLocal: Date = moment(dtIso).local().toDate();
-    //console.log(dtLocal);
-    //return dtLocal;
+    //const dt2 = moment(`${dateString}${timeString}`, "YYYY-MM-DDHH:mm:SS").local();
+    return dt;
+    //return dt2;
 };
+
+//export const combineDateAndTime = (date: Date, time: Date) => {
+
+//    //const year = date.getFullYear();
+//    //const month = date.getMonth() + 1;
+//    //const day = date.getDate();
+//    //const dateString = date.toISOString().split('T')[0];
+//    //const timeString = time.toISOString().split(' ')[0];
+
+//    //const dtIso = new Date(dateString + 'T' + timeString + '.000Z');
+//    //return dtIso;
+//    //console.log(dateString + 'T' + timeString);
+
+//    //console.log(moment(date.toISOString()).format('YYYY-MM-DD') + 'T' + moment(time.toISOString()).format('HH:mm:SS') + '.000Z');
+
+//    //const dtLocal = new Date(moment(date).format('YYYY-MM-DD') + 'T' + moment(time.toISOString()).format('HH:mm:SS') + '.000Z');
+
+//    ////const dtLocal: Date = moment(dtIso).local().toDate();
+//    //console.log(dtLocal);
+//    //return dtLocal;
+//};
