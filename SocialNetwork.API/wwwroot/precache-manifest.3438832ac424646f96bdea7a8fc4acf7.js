a0acc161ee510e508f15e2160ec90764dbc054da
Niraj Patel <nirajp82@yahoo.com>
2020-07-09 23:43:02 -0400

    Added wwwroot to API Folder

---
+++ SocialNetwork.API/wwwroot/precache-manifest.3438832ac424646f96bdea7a8fc4acf7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254c54368df20801fbf314d668fe987b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "818afabd19160351c93f",
+    "url": "/static/css/2.03499aeb.chunk.css"
+  },
+  {
+    "revision": "ca1b9e849c123e909585",
+    "url": "/static/css/main.912e9c17.chunk.css"
+  },
+  {
+    "revision": "818afabd19160351c93f",
+    "url": "/static/js/2.1a8802ce.chunk.js"
+  },
+  {
+    "revision": "c3129c3866644af74b85e5813b34daf4",
+    "url": "/static/js/2.1a8802ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1b9e849c123e909585",
+    "url": "/static/js/main.6dd3ec89.chunk.js"
+  },
+  {
+    "revision": "38f05ac45fc090c9f4ce",
+    "url": "/static/js/runtime-main.706947a8.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
