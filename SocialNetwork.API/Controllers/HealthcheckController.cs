06d1dc1032c1937064fc709434568514d2cbd284
Niraj Patel <nirajp82@yahoo.com>
2020-08-22 16:50:04 -0400

    Added Healthcheck controller

---
+++ SocialNetwork.API/Controllers/HealthcheckController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Dto;
+using System;
+using System.Threading;
+using SocialNetwork.WebUtil;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using SocialNetwork.Infrastructure;
+using SocialNetwork.Nucleus.Photo;
+using SocialNetwork.Nucleus.Followers;
+
+namespace SocialNetwork.API.Controllers
+{
+    public class HealthcheckController : BaseController
+    {
+        [HttpGet()]
+        [ProducesResponseType(typeof(IEnumerable<ProfileDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult Get()
+        {
+            return Ok("Ok");
+        }
+    }
+}
